465d02cee035d1f6c0b3177e41384fd8d37a2e1c
trojanspike <v3047-github@yahoo.co.uk>
2016-01-15 17:00:33 +0000

    user skel for node-red

--- injects/qbian-node-red/etc/skel/init.sh
+++ injects/qbian-node-red/etc/skel/init.sh
@@ -13,8 +13,9 @@ wlan0=$(cat /sys/class/net/wlan0/operstate); # up | down
 NODE_RED=$(which node-red)
 if [ "$COMMAND" == "start" ];then
 	# ~/bin/c9-install
+	# sudo-node `which node-red` -s node-red-user-dir/settings.js -u node-red-user-dir/ -v
 	# forever start ~/.c9sdk/server.js -p $PORT -l 0.0.0.0  -w $WORK_DIR --collab true --packed false -a :
-	forever start --uid "qbian-node-red-env" -a -c "sudo-node" $NODE_RED -s ~/node-red-user-dir/settings.js -u ~/ -v
+	forever start ~/forever-config.json
 fi
 
 if [ "$COMMAND" == "reload" ];then
