b79eb2b7e8fbc542fe968a7b47c1c9f86f5f49c4
trojanspike <v3047-github@yahoo.co.uk>
2016-01-21 12:11:06 +0000

    provision install required software & define node-red repo in Qbianfile

--- injects/qbian-node-red/etc/skel/init.sh
+++ injects/qbian-node-red/etc/skel/init.sh
@@ -14,8 +14,9 @@ NODE_RED=$(which node-red)
 if [ "$COMMAND" == "start" ];then
 	# ~/bin/c9-install
 	# sudo-node `which node-red` -s node-red-user-dir/settings.js -u node-red-user-dir/ -v
-	# forever start ~/.c9sdk/server.js -p $PORT -l 0.0.0.0  -w $WORK_DIR --collab true --packed false -a :
-	forever start ~/forever-config.json
+	if [ -d "~/.c9sdk" ];then
+		forever start ~/.c9sdk/server.js -p $PORT -l 0.0.0.0  -w $WORK_DIR --collab true --packed false -a :
+	fi
 fi
 
 if [ "$COMMAND" == "reload" ];then
